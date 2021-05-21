bfdaa7eb8a5441aae6491ceda4ce09100ca03d22
anvita-v <69374399+anvita-v@users.noreply.github.com>
2021-05-20 17:41:51 -0700

    Add files via upload

---
+++ crime.py
@@ -0,0 +1,25 @@
+import time
+import shutil
+
+listOfFiles = os.listdir(path)
+
+if os.path.exists(path + '/' + ext):
+        shutil.os.walk(path)
+
+else:
+    print("Not found")
+
+
+path = input(time)
+time.ctime([secs])
+
+
+ctime = os.stat(path).st_ctime
+
+return crime
+
+
+
+    
+        
+        
